f03fba16f02210ee5d0f0597ac5d17714368a6ff
Nick McDaniel <nickamcd@gmail.com>
2020-12-24 11:22:08 -0800

    Display created for secret word that matches length.

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,40 @@
 class Game
+  TURN_LIMIT = 6
+
+  def initialize(secret_word = '')
+    @secret_word = secret_word
+    guessed_letters = []
+  end
+
+  def start_game
+    @secret_word = generate_secret_word().chomp
+
+    puts @secret_word
+
+    display = Array.new(@secret_word.length)
+    display.fill('_')
+    p display
+  end
+
+  def make_guess
+
+  end
 
   def generate_secret_word
     dictionary = File.open('5desk.txt', 'r').readlines
-
     legal_words = []
 
     dictionary.each do |line|
-      line = line.chomp
-      if line.length >= 5 && line.length <= 12
+      word = line.chomp
+      if word.length >= 5 && word.length <= 12
         legal_words << line
       end
     end
 
     legal_words.sample
   end
-
 end
 
 g = Game.new
 
-g.generate_secret_word
+g.start_game
